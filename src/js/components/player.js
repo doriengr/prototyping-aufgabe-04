4c6689a78ca689041cde60e0bc542a261c2056c2
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-10-28 12:56:59 +0200

    feat: add rotation

--- src/js/components/player.js
+++ src/js/components/player.js
@@ -15,6 +15,7 @@ export default class Player {
     }
 
     addEventListener() {
+        let currentScrollY = window.scrollY;
         window.addEventListener("wheel", (e) => {
             if (this.stop) return;
 
@@ -25,6 +26,18 @@ export default class Player {
             this.playerPositionY = Math.min(maxPosition, Math.max(-200, this.playerPositionY));
 
             this.wrapper.style.top = this.playerPositionY + "px";
+
+            let rotation = 0;
+
+            if (e.deltaY < 0) {
+                rotation = -4;
+            } else if (e.deltaY > 0) {
+                rotation = 4;
+            }
+
+            this.element.style.transform = `rotate(${rotation}deg)`;
+            this.light.style.transform = `translate(0,12px) rotate(${rotation}deg)`;
+            currentScrollY = window.scrollY;
         });
     }
 }
